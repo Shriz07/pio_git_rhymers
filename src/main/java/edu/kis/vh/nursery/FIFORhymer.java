260ff6899386e977860f437e9a3c034fda1c8bc5
Adam M <adamosmakiewicz7@gmail.com>
2020-12-21 12:31:07 +0100

    1.3 poprawa bledow konwencji nazewniczej pol klas

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,19 +2,19 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends defaultCountingOutRhymer {
 
-	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+	private final defaultCountingOutRhymer TEMP = new defaultCountingOutRhymer();
 
 	@Override
 	public int countOut() {
 		while (!callCheck())
 
-			temp.countIn(super.countOut());
+			TEMP.countIn(super.countOut());
 
-		int ret = temp.countOut();
+		int ret = TEMP.countOut();
 
-		while (!temp.callCheck())
+		while (!TEMP.callCheck())
 
-			countIn(temp.countOut());
+			countIn(TEMP.countOut());
 
 		return ret;
 	}
