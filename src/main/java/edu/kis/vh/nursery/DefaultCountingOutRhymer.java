0e2e04bd2119ded70fea0e1132ad24aece1bfdc1
marcin62 <marcinkobus841@gmail.com>
2021-01-05 13:15:34 +0100

    zmiana deklaracji zmiennych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,33 +2,36 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] NUMBERS = new int[12];
+	private  final int SIZE =12;
+	private final int SIZE2=11;
+	private int[] numbers = new int[SIZE];
 
-	private int total = -1;
+	private final int EXPECTED=-1;
+	private int total = EXPECTED;
 
 	public void countIn(int in) {
 		if (!isFull())
-			NUMBERS[++total] = in;
+			numbers[++total] = in;
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == EXPECTED;
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return total == SIZE2;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
-		return NUMBERS[total];
+			return EXPECTED;
+		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
-		return NUMBERS[total--];
+			return EXPECTED;
+		return numbers[total--];
 	}
 
 }
